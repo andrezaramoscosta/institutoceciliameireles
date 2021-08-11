031988012669cbf45811aadf2c77c55f718268c4
Andreza <andreza2015r@gmail.com>
2021-08-11 16:00:15 -0300

    meu primeiro commit

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Instituto Cecilia Meireles",
+  },
+  plugins: ["gatsby-plugin-styled-components"],
+};
